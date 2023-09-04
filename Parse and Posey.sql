5b0c430591246266c29949bf736b122f74e8e5d6
Maureen Onovae <107254397+Onovae@users.noreply.github.com>
2023-09-04 12:56:08 +0100

    Update Parse and Posey.sql

--- Parse and Posey.sql
+++ Parse and Posey.sql
@@ -1,4 +1,4 @@
---SQL QUERY and ANALYSIS USING DATA FROM PARCH & POSEY (not a real company) DATABASE. 
+--SQL QUERY and ANALYSIS USING DATA FROM PARCH & POSEY DATABASE. 
 
 --Parch & Posey is a Company that sells paper and the database includes sales data for their paper. They have 50 sales reps spread accross the United States in four regions- Northwest, Southwest, West, & the Midwest. They sell three types of papers, -the regular(standard), Poster & Glossym papers.Their clients are primarily largeFortune 100 companies whom they attract by advertising on Google, Facebook and Twitter.
 --Using SQL lets help PARCH & POSEY answer some tricky questions that can help enhance performance and make decisions on channels they should make the greater investments on to improve productivity and business growth.
